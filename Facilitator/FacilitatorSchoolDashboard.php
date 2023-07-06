ab72e9d37157b00b215a9e6a528b0ef607ddb907
Justine Marie Dusaran <84701462+justinedusaran@users.noreply.github.com>
2023-07-06 12:17:08 +0800

    upload
    facilitator files

---
+++ Facilitator/FacilitatorSchoolDashboard.php
@@ -0,0 +1,115 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>wisdom-1</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,700,800&amp;display=swap">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <div>
+        <section id="sec-header" style="height: 150px;">
+            <div></div>
+        </section>
+        <section id="acc-form">
+            <div class="container">
+                <h1 style="text-align: center;font-weight: bold;margin-bottom: 39px;">School Partners</h1>
+                <div class="row" style="margin-top: 3%;">
+                    <div class="col-md-3">
+                        <div class="card" style="border-radius: 25px;border-style: none;">
+                            <div class="card-body" style="border-radius: 25px;box-shadow: 0px 4px 20px rgba(0,0,0,0.15);border-style: none;"><img width="100" height="80" style="height: 20%;width: 100%;">
+                                <h4 class="card-title" style="margin-top: 5%;text-align: center;font-weight: bold;margin-bottom: 25'px;">Pamantasan ng Lungsod ng Maynila</h4>
+                                <h6 class="text-muted card-subtitle mb-2" style="text-align: center;font-size: 14px;margin-top: 25px;margin-bottom: 30px;">General Luna, corner Muralla St, Intramuros, Manila</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card" style="border-radius: 25px;border-style: none;">
+                            <div class="card-body" style="border-radius: 25px;box-shadow: 0px 4px 20px rgba(0,0,0,0.15);border-style: none;"><img width="100" height="80" style="height: 20%;width: 100%;">
+                                <h4 class="card-title" style="margin-top: 5%;text-align: center;font-weight: bold;margin-bottom: 25'px;">Pamantasan ng Lungsod ng Maynila</h4>
+                                <h6 class="text-muted card-subtitle mb-2" style="text-align: center;font-size: 14px;margin-top: 25px;margin-bottom: 30px;">General Luna, corner Muralla St, Intramuros, Manila</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card" style="border-radius: 25px;border-style: none;">
+                            <div class="card-body" style="border-radius: 25px;box-shadow: 0px 4px 20px rgba(0,0,0,0.15);border-style: none;"><img width="100" height="80" style="height: 20%;width: 100%;">
+                                <h4 class="card-title" style="margin-top: 5%;text-align: center;font-weight: bold;margin-bottom: 25'px;">Pamantasan ng Lungsod ng Maynila</h4>
+                                <h6 class="text-muted card-subtitle mb-2" style="text-align: center;font-size: 14px;margin-top: 25px;margin-bottom: 30px;">General Luna, corner Muralla St, Intramuros, Manila</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card" style="border-radius: 25px;border-style: none;">
+                            <div class="card-body" style="border-radius: 25px;box-shadow: 0px 4px 20px rgba(0,0,0,0.15);border-style: none;"><img width="100" height="80" style="height: 20%;width: 100%;">
+                                <h4 class="card-title" style="margin-top: 5%;text-align: center;font-weight: bold;margin-bottom: 25'px;">Pamantasan ng Lungsod ng Maynila</h4>
+                                <h6 class="text-muted card-subtitle mb-2" style="text-align: center;font-size: 14px;margin-top: 25px;margin-bottom: 30px;">General Luna, corner Muralla St, Intramuros, Manila</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 3%;">
+                    <div class="col-md-3">
+                        <div class="card" style="border-radius: 25px;border-style: none;">
+                            <div class="card-body" style="border-radius: 25px;box-shadow: 0px 4px 20px rgba(0,0,0,0.15);border-style: none;"><img width="100" height="80" style="height: 20%;width: 100%;">
+                                <h4 class="card-title" style="margin-top: 5%;text-align: center;font-weight: bold;margin-bottom: 25'px;">Pamantasan ng Lungsod ng Maynila</h4>
+                                <h6 class="text-muted card-subtitle mb-2" style="text-align: center;font-size: 14px;margin-top: 25px;margin-bottom: 30px;">General Luna, corner Muralla St, Intramuros, Manila</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card" style="border-radius: 25px;border-style: none;">
+                            <div class="card-body" style="border-radius: 25px;box-shadow: 0px 4px 20px rgba(0,0,0,0.15);border-style: none;"><img width="100" height="80" style="height: 20%;width: 100%;">
+                                <h4 class="card-title" style="margin-top: 5%;text-align: center;font-weight: bold;margin-bottom: 25'px;">Pamantasan ng Lungsod ng Maynila</h4>
+                                <h6 class="text-muted card-subtitle mb-2" style="text-align: center;font-size: 14px;margin-top: 25px;margin-bottom: 30px;">General Luna, corner Muralla St, Intramuros, Manila</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card" style="border-radius: 25px;border-style: none;">
+                            <div class="card-body" style="border-radius: 25px;box-shadow: 0px 4px 20px rgba(0,0,0,0.15);border-style: none;"><img width="100" height="80" style="height: 20%;width: 100%;">
+                                <h4 class="card-title" style="margin-top: 5%;text-align: center;font-weight: bold;margin-bottom: 25'px;">Pamantasan ng Lungsod ng Maynila</h4>
+                                <h6 class="text-muted card-subtitle mb-2" style="text-align: center;font-size: 14px;margin-top: 25px;margin-bottom: 30px;">General Luna, corner Muralla St, Intramuros, Manila</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card" style="border-radius: 25px;border-style: none;">
+                            <div class="card-body" style="border-radius: 25px;box-shadow: 0px 4px 20px rgba(0,0,0,0.15);border-style: none;"><img width="100" height="80" style="height: 20%;width: 100%;">
+                                <h4 class="card-title" style="margin-top: 5%;text-align: center;font-weight: bold;margin-bottom: 25'px;">Pamantasan ng Lungsod ng Maynila</h4>
+                                <h6 class="text-muted card-subtitle mb-2" style="text-align: center;font-size: 14px;margin-top: 25px;margin-bottom: 30px;">General Luna, corner Muralla St, Intramuros, Manila</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <nav style="text-align: center;">
+                            <ul class="pagination">
+                                <li class="page-item"><a class="page-link" aria-label="Previous" href="#"><span aria-hidden="true">«</span></a></li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#">4</a></li>
+                                <li class="page-item"><a class="page-link" href="#">5</a></li>
+                                <li class="page-item"><a class="page-link" aria-label="Next" href="#"><span aria-hidden="true">»</span></a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ Facilitator/FacilitatorStudentList.php
@@ -0,0 +1,103 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>wisdom-1</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,700,800&amp;display=swap">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <div>
+        <section id="sec-header" style="height: 150px;">
+            <div></div>
+        </section>
+        <section id="table">
+            <div class="container" style="margin-right: 5.5px;margin-left: 12.5px;padding: 0px 8px;">
+                <h1 style="text-align: left;font-weight: bold;margin-bottom: 15px;margin-top: 30px;">Student List</h1>
+                <h1 style="text-align: left;font-weight: bold;margin-bottom: 15px;font-size: 30px;"><span style="font-weight: normal !important;">School Name</span></h1>
+                <p>BS Information Technology |&nbsp;Batch 1</p>
+                <div class="table-responsive" style="border-radius: 1.5rem;padding-top: 5px;border: 2px solid rgb(227,230,240);width: 100%;">
+                    <table class="table">
+                        <thead style="--bs-body-bg: #55565a;">
+                            <tr style="--bs-body-bg: #55565a;height: 56.5px;">
+                                <th style="padding-left: 30px;padding-right: 0px;padding-bottom: 15px;"><input type="checkbox" style="padding-bottom: 15px;"></th>
+                                <th style="padding-bottom: 15px;">Student Name</th>
+                                <th style="padding-bottom: 15px;">Batch</th>
+                                <th style="padding-bottom: 15px;">Status</th>
+                                <th style="padding-bottom: 15px;">Course/Strand</th>
+                                <th style="padding-bottom: 15px;">Duration</th>
+                                <th style="padding-bottom: 15px;">Hours</th>
+                                <th style="padding-bottom: 15px;">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td style="padding-left: 30px;padding-right: 0px;"><input type="checkbox"></td>
+                                <td>
+                                    <h1 style="font-size: 16px;"><strong>Student Name</strong></h1>
+                                    <p>studentemail@gmail.com</p>
+                                </td>
+                                <td>Batch 1</td>
+                                <td><button class="btn btn-primary" type="button" style="color: #CA8A04;background: rgb(254,249,195);width: 90%;border-radius: 35px;border-style: none;">Ongoing</button></td>
+                                <td>STEM</td>
+                                <td>200</td>
+                                <td>180</td>
+                                <td><button class="btn btn-primary" type="button" style="border-style: solid;border-radius: .75rem;width: 100px;padding: 10px;margin-right: 15px;"><i class="far fa-eye" style="margin-right: 5px;"></i>View</button><button class="btn btn-primary" type="button" style="background: rgb(22,163,74);border-color: rgb(22,163, 74);border-top-color: rgb(22,163,;border-right-color: 74);border-bottom-color: rgb(22,163,;border-left-color: 74);border-radius: .75rem;width: 100px;padding: 10px;"><i class="far fa-check-circle" style="margin-right: 5px;"></i>Done</button></td>
+                            </tr>
+                            <tr>
+                                <td style="padding-left: 30px;padding-right: 0px;"><input type="checkbox"></td>
+                                <td>
+                                    <h1 style="font-size: 16px;"><strong>Student Name</strong></h1>
+                                    <p>studentemail@gmail.com</p>
+                                </td>
+                                <td>Batch 1</td>
+                                <td><button class="btn btn-primary" type="button" style="color: #89c593;background: rgb(216,255,223);width: 90%;border-radius: 35px;border-style: none;">Completed</button></td>
+                                <td>STEM</td>
+                                <td>200</td>
+                                <td>200</td>
+                                <td><button class="btn btn-primary" type="button" style="border-style: solid;border-radius: .75rem;width: 100px;padding: 10px;margin-right: 15px;"><i class="far fa-eye" style="margin-right: 5px;"></i>View</button><button class="btn btn-primary" type="button" style="background: rgb(156,163,175);border-radius: .75rem;width: 100px;padding: 10px;color: rgb(255,255,255);border-color: rgb(156,163,175);border-top-color: rgb(22,163,;border-right-color: 74);border-bottom-color: rgb(22,163,;border-left-color: 74);"><i class="fas fa-check-circle" style="margin-right: 5px;"></i>Done</button></td>
+                            </tr>
+                            <tr>
+                                <td style="padding-left: 30px;padding-right: 0px;"><input type="checkbox"></td>
+                                <td>
+                                    <h1 style="font-size: 16px;"><strong>Student Name</strong></h1>
+                                    <p>studentemail@gmail.com</p>
+                                </td>
+                                <td>Batch 1</td>
+                                <td><button class="btn btn-primary" type="button" style="color: #CA8A04;background: rgb(254,249,195);width: 90%;border-radius: 35px;border-style: none;">Ongoing</button></td>
+                                <td>STEM</td>
+                                <td>200</td>
+                                <td>180</td>
+                                <td><button class="btn btn-primary" type="button" style="border-style: solid;border-radius: .75rem;width: 100px;padding: 10px;margin-right: 15px;"><i class="far fa-eye" style="margin-right: 5px;"></i>View</button><button class="btn btn-primary" type="button" style="background: rgb(22,163,74);border-color: rgb(22,163, 74);border-top-color: rgb(22,163,;border-right-color: 74);border-bottom-color: rgb(22,163,;border-left-color: 74);border-radius: .75rem;width: 100px;padding: 10px;"><i class="far fa-check-circle" style="margin-right: 5px;"></i>Done</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <nav class="text-center" style="margin-left: 40%;margin-top: 3%;margin-right: 40%;">
+                    <ul class="pagination">
+                        <li class="page-item"><a class="page-link" aria-label="Previous" href="#"><span aria-hidden="true">«</span></a></li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item"><a class="page-link" href="#">4</a></li>
+                        <li class="page-item"><a class="page-link" href="#">5</a></li>
+                        <li class="page-item"><a class="page-link" aria-label="Next" href="#"><span aria-hidden="true">»</span></a></li>
+                    </ul>
+                </nav>
+            </div>
+        </section>
+    </div>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
