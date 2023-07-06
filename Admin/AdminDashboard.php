9ae2537feeaeb70650d5169543dee7c27394098c
Justine Marie Dusaran <84701462+justinedusaran@users.noreply.github.com>
2023-07-06 12:16:21 +0800

    upload
    upload

---
+++ Admin/AdminDashboard.php
@@ -0,0 +1,86 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>wisdom-1</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,700,800&amp;display=swap">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <div>
+        <section id="sec-header" style="height: 150px;">
+            <div></div>
+        </section>
+        <section id="table">
+            <div class="container" style="border-color: rgb(233,78,80);">
+                <h1 style="text-align: left;font-weight: bold;margin-bottom: 40px;margin-top: 30px;">Account Roles</h1>
+                <div class="table-responsive" style="border-radius: 1.5rem;padding-top: 5px;border: 2px solid rgb(227,230,240);">
+                    <table class="table">
+                        <thead style="--bs-body-bg: #55565a;">
+                            <tr style="--bs-body-bg: #55565a;height: 56.5px;">
+                                <th style="padding-left: 30px;padding-right: 0px;padding-bottom: 15px;"><input type="checkbox" style="padding-bottom: 15px;"></th>
+                                <th style="padding-bottom: 15px;">Account Name</th>
+                                <th style="padding-bottom: 15px;width: 326.594px;">Email</th>
+                                <th style="width: 155px;padding-bottom: 15px;">Role</th>
+                                <th style="padding-bottom: 15px;">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td style="padding-left: 30px;padding-right: 0px;"><input type="checkbox"></td>
+                                <td>
+                                    <h1 style="font-size: 16px;">Account Name</h1>
+                                </td>
+                                <td>email@gmail.com</td>
+                                <td>Student</td>
+                                <td><button class="btn btn-primary" type="button" style="border-style: solid;border-radius: .75rem;width: 50px;padding: 10px;margin-right: 15px;"><i class="far fa-edit"></i></button><button class="btn btn-primary" type="button" style="background: rgb(233,78,80);border-radius: .75rem;width: 50px;padding: 10px;border-style: solid;border-color: #E94E50;"><i class="far fa-trash-alt"></i></button></td>
+                            </tr>
+                            <tr>
+                                <td style="padding-left: 30px;padding-right: 0px;"><input type="checkbox"></td>
+                                <td>
+                                    <h1 style="font-size: 16px;">Account Name</h1>
+                                </td>
+                                <td>email@gmail.com</td>
+                                <td>Student</td>
+                                <td><button class="btn btn-primary" type="button" style="border-style: solid;border-radius: .75rem;width: 50px;padding: 10px;margin-right: 15px;"><i class="far fa-edit"></i></button><button class="btn btn-primary" type="button" style="background: rgb(233,78,80);border-radius: .75rem;width: 50px;padding: 10px;border-style: solid;border-color: #E94E50;"><i class="far fa-trash-alt"></i></button></td>
+                            </tr>
+                            <tr>
+                                <td style="padding-left: 30px;padding-right: 0px;"><input type="checkbox"></td>
+                                <td>
+                                    <h1 style="font-size: 16px;">Account Name</h1>
+                                </td>
+                                <td>email@gmail.com</td>
+                                <td>Student</td>
+                                <td><button class="btn btn-primary" type="button" style="border-style: solid;border-radius: .75rem;width: 50px;padding: 10px;margin-right: 15px;"><i class="far fa-edit"></i></button><button class="btn btn-primary" type="button" style="background: rgb(233,78,80);border-radius: .75rem;width: 50px;padding: 10px;border-style: solid;border-color: #E94E50;"><i class="far fa-trash-alt"></i></button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <nav class="text-center" style="margin-left: 40%;margin-top: 3%;margin-right: 40%;">
+                    <ul class="pagination">
+                        <li class="page-item"><a class="page-link" aria-label="Previous" href="#"><span aria-hidden="true">«</span></a></li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item"><a class="page-link" href="#">4</a></li>
+                        <li class="page-item"><a class="page-link" href="#">5</a></li>
+                        <li class="page-item"><a class="page-link" aria-label="Next" href="#"><span aria-hidden="true">»</span></a></li>
+                    </ul>
+                </nav>
+            </div>
+        </section>
+    </div>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ Admin/student-application.php
@@ -0,0 +1,118 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Page Not Found - Brand</title>
+    <link rel="stylesheet" href="admin-assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;display=swap">
+    <link rel="stylesheet" href="admin-assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="admin-assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="admin-assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="admin-assets/css/2-columns-media-image-video-carousel-map-2-columns-media1.css">
+    <link rel="stylesheet" href="admin-assets/css/Bold-BS4-Animated-Back-To-Top.css">
+    <link rel="stylesheet" href="admin-assets/css/Bootstrap-Cards-v2.css">
+    <link rel="stylesheet" href="admin-assets/css/Documents-App-Browser.css">
+    <link rel="stylesheet" href="admin-assets/css/iframe.css">
+    <link rel="stylesheet" href="admin-assets/css/untitled.css">
+</head>
+
+<body id="page-top">
+    <div id="wrapper">
+        <nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0" style="background: #69c9ff;">
+            <div class="container-fluid d-flex flex-column p-0"><a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="school-applicant.php">
+                    <div class="sidebar-brand-icon rotate-n-15"></div>
+                    <div class="sidebar-brand-text mx-3"><span><img src="admin-assets/img/New%20Project%20(1).png" width="106" height="27"></span></div>
+                </a>
+                <hr class="sidebar-divider my-0">
+                <ul class="navbar-nav text-light" id="accordionSidebar">
+                    <li class="nav-item"><a class="nav-link" href="school-applicant.php"><i class="fa fa-briefcase"></i><span>School Applicant</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="daily-log.php"><i class="fa fa-pencil-square-o"></i><span>Intern Daily Log</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="calendar.php"><i class="fa fa-calendar-plus-o"></i><span>Intern Calendar</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="documentation.php"><i class="fa fa-folder-open"></i><span>Intern Documentation</span></a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="d-flex flex-column" id="content-wrapper">
+            <div id="content">
+                <nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
+                    <div class="container-fluid"><button class="btn btn-link d-md-none rounded-circle me-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button>
+                        <form class="d-none d-sm-inline-block me-auto ms-md-3 my-2 my-md-0 mw-100 navbar-search">
+                            <div class="input-group"></div>
+                        </form>
+                        <ul class="navbar-nav flex-nowrap ms-auto">
+                            <div class="d-none d-sm-block topbar-divider"></div>
+                            <li class="nav-item dropdown no-arrow">
+                                <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><span class="d-none d-lg-inline me-2 text-gray-600 small">Company Name</span><img class="border rounded-circle img-profile" src="admin-assets/img/avatars/side%20logo.png" width="32" height="32"></a>
+                                    <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in"><a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Logout</a></div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <div class="text-center" id="school-info" style="padding-right: 24px;padding-left: 24px;"><img style="width: 100px;" src="https://4.bp.blogspot.com/-YZxFNaiint4/WOL95a6PLkI/AAAAAAAAAMs/WgHDfKoN9ocGbcnooOb-tLmKLoAVt7GaACLcB/s1600/TIP%2BTECHNOLOGY%2BINSTITUTE%2BOF%2BTHE%2BPHILIPPINES.png">
+                    <h5 id="school-name" style="color: rgb(0,0,0);font-weight: bold;padding-top: 5px;margin-bottom: 0px;">Technological Institute of the Philippines</h5>
+                    <p id="school-location-1" style="margin-bottom: 0px;">363 Pascual Casal St, Quiapo, Manila</p>
+                    <p id="school-email">manila@tip.edu.ph.</p>
+                    <div>
+                        <div class="container text-center" style="width: 814px;">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <p id="school-location" style="margin-bottom: 0px;">363 Pascual Casal St, Quiapo, Manila</p>
+                                </div>
+                                <div class="col-md-6">
+                                    <p id="school-email-1">manila@tip.edu.ph.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="body">
+                    <div class="container">
+                        <div id="main-content">
+                            <div class="container" style="padding-bottom: 10px;">
+                                <div class="row" id="internship-body" style="background: #f2f2f2;color: rgb(0,0,0);">
+                                    <div class="col-md-6">
+                                        <div id="school-name-1" style="margin-top: 7px;"><a id="program-name" href="#" style="font-size: 18px;color: rgb(0,0,0);">Student Name</a></div>
+                                        <p style="font-size: 13px;color: rgb(85,85,85);margin-bottom: 7px;">Year/Course</p>
+                                    </div>
+                                    <div class="col" id="colb">
+                                        <div id="divb"><button class="btn btn-primary" id="accept" type="button"><i class="fa fa-check-circle" style="padding-right: 5px;"></i>Proceed</button>
+                                            <div id="divider"></div><button class="btn btn-primary" id="reject" type="button"><i class="fa fa-times-circle" style="padding-right: 5px;"></i>Decline</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="container" style="padding-bottom: 10px;">
+                                <div class="row" id="internship-body" style="background: #f2f2f2;color: rgb(0,0,0);">
+                                    <div class="col-md-6">
+                                        <div id="school-name-1" style="margin-top: 7px;"><a id="program-name" href="#" style="font-size: 18px;color: rgb(0,0,0);">Student Name</a></div>
+                                        <p style="font-size: 13px;color: rgb(85,85,85);margin-bottom: 7px;">Address</p>
+                                    </div>
+                                    <div class="col" id="colb">
+                                        <div id="divb"><button class="btn btn-primary" id="accept" type="button"><i class="fa fa-check-circle" style="padding-right: 5px;"></i>Proceed</button>
+                                            <div id="divider"></div><button class="btn btn-primary" id="reject" type="button"><i class="fa fa-times-circle" style="padding-right: 5px;"></i>Decline</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="back-to-top"><a class="cd-top js-cd-top cd-top--fade-out cd-top--show" style="background-image: url(&quot;admin-assets/img/cd-top-arrow.svg&quot;);border-radius: 50px;" href="#0">Top</a></div>
+        </div>
+    </div>
+    <script src="admin-assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="admin-assets/js/2-columns-media-image-video-carousel-map-2-columns-media.js"></script>
+    <script src="admin-assets/js/Bold-BS4-Animated-Back-To-Top-backtotop.js"></script>
+    <script src="admin-assets/js/theme.js"></script>
+</body>
+
+</html>
