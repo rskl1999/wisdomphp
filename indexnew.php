65460f4d81bd7ecae5f90ce3ccdc19a1e7fb1d54
Llivi Cai <138189751+Llivi-Jaaz@users.noreply.github.com>
2023-07-11 09:03:10 +0800

    Add files via upload

---
+++ indexnew.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- header-->
+<?php include 'includes/header.php'; ?>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>landing</title>
+    <link rel="stylesheet" href="landing-assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Architects+Daughter&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat+Alternates:400,600&amp;display=swap">
+    <link rel="stylesheet" href="landing-assets/css/aos.min.css">
+    <link rel="stylesheet" href="landing-assets/css/animate.min.css">
+    <link rel="stylesheet" href="landing-assets/css/2-columns-media-image-video-carousel-map-2-columns-media1.css">
+    <link rel="stylesheet" href="landing-assets/css/Banner-Heading-Image-images.css">
+    <link rel="stylesheet" href="landing-assets/css/Navbar-Right-Links-icons.css">
+    <link rel="stylesheet" href="landing-assets/css/Video-Parallax-Background-v2-multiple-parallax.css">
+</head>
+
+<body>
+    <div class="parallax" style="height: 100vh;">
+        
+        <div class="container d-flex justify-content-center align-items-center parallax-content">
+            <div class="col-12 col-md-10 col-lg-8 d-flex justify-content-center flex-column" style="width: 908.656px;">
+                <h1 class="pulse animated" style="font-family: 'Montserrat Alternates', sans-serif;font-size: 34px;"><strong>Creating a world of endless possibilities</strong></h1>
+                <p class="pulse animated" style="font-size: 24px;font-family: 'Montserrat Alternates', sans-serif;">Let's turn our visions into reality and create a new world</p>
+            </div>
+        </div><video class="parallax-background" autoplay="" loop="" muted="">
+            <source src="landing-assets/video/cubes.mp4" type="video/mp4" wp-acf="[{'type':'url','name':'video','label':'Video','wrapper':{'width':25}},{'type':'text','name':'video_css','label':'Video CSS (eg. filters)','wrapper':{'width':25}}]" wp-attr="[{'target':'src','replace':'%1'},{'target':'parent_style','replace':'%2'}]">
+        </video>
+    
+    </div>
+    <div id="abt-1" style="height: 100vh;background: #6ca3a1;">
+        <section style="background: #262426;">
+            <div class="container" style="height: 100%;background: #6ca3a1;">
+                <div class="row row-media">
+                    <div class="col-md-6 column-text" style="color: rgb(255,255,255);">
+                        <h2 class="text-center" style="color: rgb(17,74,72);font-family: Montserrat, sans-serif;font-weight: bold;">ROBOTICS</h2>
+                        <p class="text-justify" style="color: rgb(17,74,72);font-family: Montserrat, sans-serif;">We are a team of robotics experts who design and create innovative solutions for businesses and organizations, using our multidisciplinary approach to improve efficiency and achieve goals.</p>
+                        <p class="text-center"><a class="btn btn-primary" role="button" style="height: 40px;width: 108.5px;font-family: Montserrat, sans-serif;font-weight: bold;border-width: 0px;background: rgb(17,74,72);color: rgb(108,163,161);">Join Now</a></p>
+                    </div>
+                    <div class="col-md-6 column-media"><img class="img-fluid" data-aos="zoom-in" alt="Lorem" src="landing-assets/img/431fdf9cd1c08057a56c65607d0e6599.gif"></div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <div id="abt-2" style="height: 100vh;background: #fadd91;">
+        <section style="background: #fadd91;">
+            <div class="container" style="height: 100%;">
+                <div class="row row-media">
+                    <div class="col-md-6 column-text" style="color: rgb(255,255,255);">
+                        <h2 class="text-center" style="color: rgb(88,48,34);font-family: Montserrat, sans-serif;font-weight: bold;">3D PRINTING</h2>
+                        <p class="text-justify" style="color: rgb(89,47,33);font-family: Montserrat, sans-serif;">We are a team of passionate animators who create visually stunning content using a wide range of animation techniques and software. Our goal is to bring our clients' visions to life through the magic of animation.</p>
+                        <p class="text-center"><a class="btn btn-primary" role="button" style="height: 40px;width: 108.5px;font-family: Montserrat, sans-serif;font-weight: bold;border-width: 0px;background: rgb(91,49,35);color: rgb(250,221,145);">Join Now</a></p>
+                    </div>
+                    <div class="col-md-6 column-media"><img class="img-fluid" data-aos="zoom-in" alt="Lorem" src="landing-assets/img/3d.gif"></div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <div id="abt-3" style="height: 100vh;background: #bcccfe;">
+        <section style="background: #bcccfe;">
+            <div class="container" style="height: 100%;">
+                <div class="row row-media">
+                    <div class="col-md-6 column-media"><img class="img-fluid" data-aos="zoom-in" alt="Lorem" style="border-color: rgba(33,37,41,0);" src="landing-assets/img/developers-gif-showcase.gif"></div>
+                    <div class="col-md-6 column-text" style="color: rgb(255,255,255);">
+                        <h2 class="text-center" style="font-family: Montserrat, sans-serif;color: rgb(95,107,221);"><strong>PROGRAMMING</strong></h2>
+                        <p class="text-justify" style="font-family: Montserrat, sans-serif;color: rgb(95,107,221);">We are a team of programming specialists who use our expertise to create custom software solutions for businesses and organizations, helping our clients succeed in today's rapidly evolving technological landscape.</p>
+                        <p class="text-center"><a class="btn btn-primary" role="button" style="color: rgb(188,204,254);background: rgb(95,107,221);width: 108.5px;height: 40px;font-family: Montserrat, sans-serif;border-width: 0px;font-weight: bold;">Join Now</a></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <div id="abt-4" style="height: 100vh;background: #f7ffff;">
+        <section style="background: #f7ffff;">
+            <div class="container" style="height: 100%;">
+                <div class="row row-media">
+                    <div class="col-md-6 column-media"><img class="img-fluid" data-aos="zoom-in" alt="Lorem" src="landing-assets/img/1_MptjiGxY9vSRW4ZC9S7Isw.gif"></div>
+                    <div class="col-md-6 column-text" style="color: rgb(255,255,255);">
+                        <h2 class="text-center" style="font-family: Montserrat, sans-serif;color: rgb(194,159,242);"><strong>GRAPHIC DESIGN</strong></h2>
+                        <p class="text-justify" style="font-family: Montserrat, sans-serif;color: rgb(194,159,242);">We are a team of passionate animators who create visually stunning content using a wide range of animation techniques and software. Our goal is to bring our clients' visions to life through the magic of animation.</p>
+                        <p class="text-center"><a class="btn btn-primary" role="button" style="color: rgb(247,255,255);background: rgb(194,159,242);width: 108.5px;height: 40px;font-family: Montserrat, sans-serif;border-width: 0px;font-weight: bold;">Join Now</a></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <div id="abt" style="height: 100vh;background: #262426;">
+        <section style="background: #262426;">
+            <div class="container" style="height: 100%;">
+                <div class="row row-media">
+                    <div class="col-md-6 column-media"><img class="img-fluid" data-aos="zoom-in" alt="Lorem" src="landing-assets/img/ezgif.com-video-to-gif.gif"></div>
+                    <div class="col-md-6 column-text" style="color: rgb(255,255,255);">
+                        <h2 class="text-center" style="font-family: Montserrat, sans-serif;"><strong>ANIMATION</strong></h2>
+                        <p class="text-justify" style="font-family: Montserrat, sans-serif;">We are a team of passionate animators who create visually stunning content using a wide range of animation techniques and software. Our goal is to bring our clients' visions to life through the magic of animation.</p>
+                        <p class="text-center"><a class="btn btn-primary" role="button" style="color: rgb(38,36,38);background: rgb(255,255,255);width: 108.5px;height: 40px;font-family: Montserrat, sans-serif;border-width: 0px;font-weight: bold;">Join Now</a></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <script src="landing-assets/js/jquery.min.js"></script>
+    <script src="landing-assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="landing-assets/js/aos.min.js"></script>
+    <script src="landing-assets/js/bs-init.js"></script>
+    <script src="landing-assets/js/2-columns-media-image-video-carousel-map-2-columns-media.js"></script>
+    <script src="landing-assets/js/Video-Parallax-Background-v2-multiple-parallax.js"></script>
+
+<!-- footer-->
+<?php include 'includes/footer.php'; ?>
+
+</body> 
+</html>
