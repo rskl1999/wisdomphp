8dd33ffcfd4308498266b0678d5542ef03031db5
Justine Marie Dusaran <84701462+justinedusaran@users.noreply.github.com>
2023-07-12 09:50:47 +0800

    updated assets

---
+++ Facilitator/assets/js/Simple-Slider.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Initializing the swiper plugin for the slider.
+    // Read more here: http://idangero.us/swiper/api/
+    
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+        slidesPerView: 4,
+        spaceBetween: 20,
+        pagination: {
+            el: '.swiper-pagination' ,
+            clickable: true
+        },
+        paginationClickable: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
+    });
+    
+});
