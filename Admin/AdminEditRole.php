4cc0c2f19abef86ea79afa54c6a1621867360d5b
Justine Marie Dusaran <84701462+justinedusaran@users.noreply.github.com>
2023-07-12 16:07:49 +0800

    Update AdminEditRole.php

--- Admin/AdminEditRole.php
+++ Admin/AdminEditRole.php
@@ -10,18 +10,37 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>wisdom-1</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,700,800&amp;display=swap">
-    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="admin-assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins&amp;display=swap">
+    <link rel="stylesheet" href="admin-assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="admin-assets/css/Bootstrap-Cards-v2.css">
+    <link rel="stylesheet" href="admin-assets/css/Drag--Drop-Upload-Form.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.2/css/theme.bootstrap_4.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css">
+    <link rel="stylesheet" href="admin-assets/css/Ludens---1-Index-Table-with-Search--Sort-Filters-v20.css">
+    <link rel="stylesheet" href="admin-assets/css/Navbar-With-Button-icons.css">
+    <link rel="stylesheet" href="admin-assets/css/Profile-Edit-Form-styles.css">
+    <link rel="stylesheet" href="admin-assets/css/Profile-Edit-Form.css">
 </head>
 
-<body>
-    <div>
-        <section id="sec-header" style="height: 150px;">
-            <div>
-                <h1 class="fw-bold" style="padding-top: 80px;padding-left: 110px;">Edit Account Role</h1>
-            </div>
-        </section>
+<body style="color: rgb(0,0,0);">
+    <nav class="navbar navbar-light navbar-expand bg-white  topbar static-top">
+    <div class="container-fluid"><a href="StudentDashboard.php"><img src="assets/img/logo.png" width="140" height="29" /></a>
+        <ul class="navbar-nav flex-nowrap ms-auto">
+            <li class="nav-item dropdown no-arrow mx-1"></li>
+            <li class="nav-item dropdown no-arrow">
+                <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><img class="border rounded-circle img-profile" src="admin-assets/img/avatars/avatar1.jpeg" /></a>
+                    <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in"><a class="dropdown-item" href="StudentProfile.php"><i class="fas fa-user fa-sm fa-fw me-2 text-gray-400"></i> Edit Profile</a><a class="dropdown-item" href="StudentDashboard.php"><i class="fas fa-home fa-sm fa-fw me-2 text-gray-400"></i>Dashboard</a>
+                        <div class="dropdown-divider"></div><a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i> Logout</a>
+                    </div>
+                </div>
+            </li>
+        </ul>
+        </div>
+        </nav>
+        <div id="banner" style="height: 250px;background: url(&quot;assets/img/banner.jpg&quot;) center / cover no-repeat, #d3edff;">
+        </div>
         <section id="admin-edit-acc">
             <form style="margin: 50px;">
                 <div>
