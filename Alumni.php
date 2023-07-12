e697da6604225c5e335d57aea16755dade5182a5
Llivi Cai <138189751+Llivi-Jaaz@users.noreply.github.com>
2023-07-12 11:27:41 +0800

    Add files via upload

---
+++ Alumni.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- header-->
+<?php include 'includes/header.php'; ?>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>wisdom</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:400,600,800">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,700&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,400i,700,700i">
+    <link rel="stylesheet" href="assets/css/swiper-icons.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/Features-Cards-icons.css">
+    <link rel="stylesheet" href="assets/css/Landing%20Page.css">
+    <link rel="stylesheet" href="assets/css/Simple-Slider-swiper-bundle.min.css">
+    <link rel="stylesheet" href="assets/css/Simple-Slider.css">
+    <link rel="stylesheet" href="assets/css/Tasks.css">
+    <link rel="stylesheet" href="assets/css/Ultimate-Event-Calendar.css">
+    
+
+    
+</head>
+
+<body>
+    <div style="font-family: Poppins, sans-serif;">
+        <!-- <section style="background: url(&quot;assets/img/aboutBg.svg&quot;) top / cover; height: 1455px;"> -->
+        <!-- <div class="black-rectangle" style="position: absolute; top: 95em; left: 0; width: 100%; height: 24.5rem; background-color: #000;"></div> -->
+        <div class="container" style="width: 1000px; max-width: 1500px; position: relative; margin: 0 auto; top: 50px; text-align: center;">
+        <h1 class="abt-tk" style="font-weight: bold; font-size: 80px;">Why join <span style="color: #0017EB;">Technokids</span></h1>
+        <br>
+        </div>
+    </div>
+        
+    <div style="font-family: Poppins, bold;">
+        <div class="container" style="width: 1000px; max-width: 1500px; position: relative; margin: 0 auto; top: 100px; text-align: center; display: grid; grid-template-columns: 1fr 1fr 1fr; gap: 90px;">
+            <div>
+            <div></div><i class="fa-solid fa-badge-check" style="font-size: 21px;color: #3448ff;width: 30px;"></i>
+            <h1 class="abt-tk" style="font-weight: bold; font-size: 30px;">Guaranteed</h1>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 15px; margin: 0 auto;">PCMed-TechnoKids, conceptualized in 2002, is a premier computer curriculum service provider in the </h2>
+            <br>
+            <br>
+            <h1 class="abt-tk" style="font-weight: bold; font-size: 30px;">Strong Together</h1>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 15px; margin: 0 auto;">PCMed-TechnoKids, conceptualized in 2002, is a premier computer curriculum service provider in the </h2>
+            </div>
+
+            <div>
+            <h1 class="abt-tk" style="font-weight: bold; font-size: 30px;">Learning</h1>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 15px; margin: 0 auto;">PCMed-TechnoKids, conceptualized in 2002, is a premier computer curriculum service provider in the </h2>
+            <br>
+            <br>
+            <h1 class="abt-tk" style="font-weight: bold; font-size: 30px;">Professional</h1>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 15px; margin: 0 auto;">PCMed-TechnoKids, conceptualized in 2002, is a premier computer curriculum service provider in the </h2>
+            </div>
+            
+            <div>
+            <h1 class="abt-tk" style="font-weight: bold; font-size: 30px;">Achievement</h1>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 15px; margin: 0 auto;">PCMed-TechnoKids, conceptualized in 2002, is a premier computer curriculum service provider in the </h2>
+            <br>
+            <br>
+            <h1 class="abt-tk" style="font-weight: bold; font-size: 30px;">Big Network</h1>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 15px; margin: 0 auto;">PCMed-TechnoKids, conceptualized in 2002, is a premier computer curriculum service provider in the </h2>
+            </div>
+        </div>
+    </div>
+
+
+    <div>
+        <section style="background: url(&quot;assets/img/Group%2033527.svg&quot;) top / cover;height: 1200px;padding-top: 8rem;">
+            <div class="carousel slide" data-bs-ride="false" id="carousel-1" style="margin-right: auto;margin-left: auto;max-width: 850px;text-align: center; min-width: 330px; ">
+                <div class="carousel-inner" style="text-align: center;">
+                    <div class="carousel-item active" style="max-width: 850px;">
+                        <div style="height: 960px; position:relative;">
+                            <div id="slide-responsive" style="top: 23rem;background: #373737;position: absolute;border-radius: 15px;width: 100%;height: auto;">
+                                <h1 class="fw-normal" style="margin-top: 9rem;color: rgb(255,255,255);margin-left: 7rem;margin-right: 7rem;font-size: 20px;margin-bottom: 8rem;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec eros auctor, placerat odio et, sollicitudin sapien. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.&nbsp;</h1>
+                                <div style="left: 0;bottom: 0;background: rgba(31,31,31,0.9);border-bottom-right-radius: 15px;border-bottom-left-radius: 15px;position: absolute;width: 100%;">
+                                    <h1 style="color: rgb(255,255,255);font-size: 23px;margin-top: 10px;">Jinnah</h1>
+                                    <h1 class="fw-lighter" style="color: rgb(255,255,255);font-size: 21px;margin-top: 0px;">Programming Intern</h1>
+                                </div>
+                            </div><img class="rounded-circle img-fluid" src="assets/img/clipboard-image.png" alt="Slide Image" width="850" height="201" style="object-fit: cover;text-align: center;width: 200px;height: 200px;transform: perspective(0px) translateY(260px);">
+                        </div>
+                    </div>
+                    <div class="carousel-item" style="max-width: 850px;">
+                        <div style="height: 960px; position:relative;">
+                            <div id="slide-responsive" style="top: 23rem;background: #373737;position: absolute;border-radius: 15px;width: 100%;height: auto;">
+                                <h1 class="fw-normal" style="margin-top: 9rem;color: rgb(255,255,255);margin-left: 7rem;margin-right: 7rem;font-size: 20px;margin-bottom: 8rem;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et nunc lacus."</h1>
+                                <div style="left: 0;bottom: 0;background: rgba(31,31,31,0.9);border-bottom-right-radius: 15px;border-bottom-left-radius: 15px;position: absolute;width: 100%;">
+                                    <h1 style="color: rgb(255,255,255);font-size: 23px;margin-top: 10px;">FirstName Last Name</h1>
+                                    <h1 class="fw-lighter" style="color: rgb(255,255,255);font-size: 21px;margin-top: -14px;">Position</h1>
+                                </div>
+                            </div><img class="rounded-circle img-fluid" src="assets/img/18724b620dcf8ff579dcb99cb1599f21.webp" alt="Slide Image" width="850" height="201" style="object-fit: cover;text-align: center;width: 200px;height: 200px;transform: perspective(0px) translateY(260px);">
+                        </div>
+                    </div>
+                    <div class="carousel-item" style="max-width: 850px;">
+                        <div style="height: 960px; position:relative;">
+                            <div id="slide-responsive" style="top: 23rem;background: #373737;position: absolute;border-radius: 15px;width: 100%;height: auto;">
+                                <h1 class="fw-normal" style="margin-top: 9rem;color: rgb(255,255,255);margin-left: 7rem;margin-right: 7rem;font-size: 20px;margin-bottom: 8rem;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et nunc lacus."</h1>
+                                <div style="left: 0;bottom: 0;background: rgba(31,31,31,0.9);border-bottom-right-radius: 15px;border-bottom-left-radius: 15px;position: absolute;width: 100%;">
+                                    <h1 style="color: rgb(255,255,255);font-size: 23px;margin-top: 10px;">FirstName Last Name</h1>
+                                    <h1 class="fw-lighter" style="color: rgb(255,255,255);font-size: 21px;margin-top: -14px;">Position</h1>
+                                </div>
+                            </div><img class="rounded-circle img-fluid" src="assets/img/18724b620dcf8ff579dcb99cb1599f21.webp" alt="Slide Image" width="850" height="201" style="object-fit: cover;text-align: center;width: 200px;height: 200px;transform: perspective(0px) translateY(260px);">
+                        </div>
+                    </div>
+                </div>
+                <div><a class="carousel-control-prev" href="#carousel-1" role="button" data-bs-slide="prev"><span class="carousel-control-prev-icon"></span><span class="visually-hidden">Previous</span></a><a class="carousel-control-next" href="#carousel-1" role="button" data-bs-slide="next"><span class="carousel-control-next-icon"></span><span class="visually-hidden">Next</span></a></div>
+                <ol class="carousel-indicators" style="display: none;">
+                    <li data-bs-target="#carousel-1" data-bs-slide-to="0" class="active"></li>
+                    <li data-bs-target="#carousel-1" data-bs-slide-to="1"></li>
+                    <li data-bs-target="#carousel-1" data-bs-slide-to="2"></li>
+                </ol>
+            </div>
+        </section>
+    </div>
+    
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="assets/js/Card-Carousel-slider.js"></script>
+    <script src="assets/js/Daily-Logs-and-Tasks-Toggle.js"></script>
+    <script src="assets/js/Simple-Slider-swiper-bundle.min.js"></script>
+    <script src="assets/js/Simple-Slider.js"></script>
+    <script src="assets/js/Task-List.js"></script>
+    <script src="assets/js/Ultimate-Event-Calendar.js"></script>
+    
+<!-- footer-->
+<?php include 'includes/footer.php'; ?>
+</body>
+
+</html>
