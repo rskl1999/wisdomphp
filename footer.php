56ef9d9fd4edca4260fe449ac75ea393c0f26d9e
Llivi Cai <138189751+Llivi-Jaaz@users.noreply.github.com>
2023-07-12 11:21:46 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins&amp;display=swap">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.2/css/theme.bootstrap_4.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css">
+    <link rel="stylesheet" href="assets/css/Ludens---1-Index-Table-with-Search--Sort-Filters-v20.css">
+    <link rel="stylesheet" href="assets/css/Navbar-With-Button-icons.css">
+    <link rel="stylesheet" href="landing-assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="includes/footer.css">
+    
+
+    
+</head>
+
+<body>
+    <footer style="background: #1B3996;color: rgb(255,255,255);">
+        <div class="container-fluid py-4 py-lg-5">
+            <div class="row justify-content-center">
+                <div class="col-sm-4 col-md-3 text-center text-lg-start d-flex flex-column">
+                    <h3 class="fs-6"><img src="assets/img/logo_white.png" width="140" height="29"></h3>
+                    <p style= "text-align: justify";>FortWorth Inc. (PCMed - Technokids Philippines) is a premier I.T. company in the country specializing in I.T. solutions and providing I.T. education to our partners.</p>
+
+                    <!--<h3 class="fs-6"><strong>Follow Us</strong></h3> -->
+                    <div class="footer-col">
+                    <div class="social-links">
+  	 				<a href="https://www.facebook.com/TechnokidsPh" target="_blank"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="https://twitter.com/technokidsinc" target="_blank"><i class="fab fa-twitter"></i></a>
+  	 				<a href="https://www.instagram.com/technokidsphilippines/" target="_blank"><i class="fab fa-instagram"></i></a>
+  	 				<a href="https://www.linkedin.com/company/technokids-philippines/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+
+  	 			</div>
+                </div>
+                <div class="col-sm-4 col-md-3 text-center text-lg-start d-flex flex-column"></div>
+                <div class="col-sm-4 col-md-3 text-center text-lg-start d-flex flex-column">
+                    <h3 class="fs-6"><strong>Our Company</strong></h3>
+                    <div class="footer-col">
+                        <ul class="list-unstyled">
+                            <li> <a href="index.php" class="text-white text-decoration-none fs-6">Home </a></li>
+                            <li> <a href="About Us.php" class="text-white text-decoration-none fs-6">About </a></li>
+                            <li> <a href="" class="text-white text-decoration-none fs-6">Categories</li>
+                            <li> <a href="Alumni.php" class="text-white text-decoration-none fs-6">Alumni</a></li>
+                            <li> <a href="Contact Us.php" class="text-white text-decoration-none fs-6">Contact</a></li>
+                         </ul>
+                    </div>
+                </div>
+
+                <div class="col-sm-4 col-md-3 text-center text-lg-start d-flex flex-column">
+                    <h3 class="fs-6"><strong>Categories</strong></h3>
+                    <div class="footer-col">
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="text-white text-decoration-none fs-6">Robotics </a></li>
+                            <li><a href="#" class="text-white text-decoration-none fs-6">3D Printing </a></li>
+                            <li><a href="#" class="text-white text-decoration-none fs-6">Programming </a></li>
+                            <li><a href="#" class="text-white text-decoration-none fs-6">Graphic Designing </a></li>
+                            <li><a href="#" class="text-white text-decoration-none fs-6">Animation </a></li>
+                    
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+      
+        <div style="height:40px; background: #122664;margin-top: 56px;">
+            <p class="text-center" style="color: white; padding-top: 10px;"><i class="far fa-copyright"></i>&nbsp;All Copyrights are reserved by Wisdom.</p>
+        </div>
+
+    </footer>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.2/js/jquery.tablesorter.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.2/js/widgets/widget-filter.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.2/js/widgets/widget-storage.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="assets/js/Ludens---1-Index-Table-with-Search--Sort-Filters-v20-Ludens---1-Index-Table-with-Search--Sort-Filters.js"></script>
+    <script src="assets/js/Ludens---1-Index-Table-with-Search--Sort-Filters-v20-Ludens---Material-UI-Actions.js"></script>
+    <script src="assets/js/theme.js"></script>
+
+
+</body>
+
+</html>
