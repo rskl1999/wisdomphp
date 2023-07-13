2ab43da52c403b8820b3a604e3a05f96219f3e43
Justine Marie Dusaran <84701462+justinedusaran@users.noreply.github.com>
2023-07-13 10:26:39 +0800

    Update FacilitatorStudentList.php

--- Facilitator/FacilitatorStudentList.php
+++ Facilitator/FacilitatorStudentList.php
@@ -9,19 +9,19 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>wisdom-1</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <title>Facilitator Student List</title>
+    <link rel="stylesheet" href="facilitator-assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins&amp;display=swap">
-    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
-    <link rel="stylesheet" href="assets/css/Bootstrap-Cards-v2.css">
-    <link rel="stylesheet" href="assets/css/Drag--Drop-Upload-Form.css">
+    <link rel="stylesheet" href="facilitator-assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="facilitator-assets/css/Bootstrap-Cards-v2.css">
+    <link rel="stylesheet" href="facilitator-assets/css/Drag--Drop-Upload-Form.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.2/css/theme.bootstrap_4.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css">
-    <link rel="stylesheet" href="assets/css/Ludens---1-Index-Table-with-Search--Sort-Filters-v20.css">
-    <link rel="stylesheet" href="assets/css/Navbar-With-Button-icons.css">
-    <link rel="stylesheet" href="assets/css/Profile-Edit-Form-styles.css">
-    <link rel="stylesheet" href="assets/css/Profile-Edit-Form.css">
+    <link rel="stylesheet" href="facilitator-assets/css/Ludens---1-Index-Table-with-Search--Sort-Filters-v20.css">
+    <link rel="stylesheet" href="facilitator-assets/css/Navbar-With-Button-icons.css">
+    <link rel="stylesheet" href="facilitator-assets/css/Profile-Edit-Form-styles.css">
+    <link rel="stylesheet" href="facilitator-assets/css/Profile-Edit-Form.css">
 </head>
 
 <body style="color: rgb(0,0,0);"><nav class="navbar navbar-light navbar-expand bg-white  topbar static-top">
@@ -115,7 +115,7 @@
                 </nav>
             </div>
         </section>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="facilitator-assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
 
 </html>
