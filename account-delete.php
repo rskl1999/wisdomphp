84a5e1ad102ffc6329e0b2637291f45c5915b05a
Llivi Cai <138189751+Llivi-Jaaz@users.noreply.github.com>
2023-07-17 13:42:59 +0800

    Update account-delete.php

--- account-delete.php
+++ account-delete.php
@@ -1,5 +1,25 @@
-<?php
+<?php 
 
-    echo "success";
+$user = "root";         //USERNAME
+$pass = "";             //PASSWORD
+$host = "localhost";    //SERVERNAME
+$db_name = "wisdomdb";  //DATABASE NAME
+$con = mysqli_connect ($host, $user, $pass);
+$db = mysqli_select_db ($con, $db_name );
+
+if(isset($_GET['deleteid'])){
+    $account_id=$_GET['deleteid'];
+
+    $sql="DELETE FROM accounttbl WHERE account_id=$account_id";
+    $result=mysqli_query($con,$sql);
+
+    if($result){
+    echo "Deleted successfully";
+        //header('<location:Admin/AdminDashboard.php');
+    }
+    else{
+        die(mysqli_error($con));
+    }
+}
 
 ?>
