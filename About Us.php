302bf851f2f9b90650b18cb3f863173713131dee
Llivi Cai <138189751+Llivi-Jaaz@users.noreply.github.com>
2023-07-29 21:40:59 +0800

    Update About Us.php

--- About Us.php
+++ About Us.php
@@ -28,61 +28,62 @@
         <div class="black-rectangle" style="position: absolute; top: 95em; left: 0; width: 100%; height: 24.5rem; background-color: #000;"></div>
             <div class="container" style="width: 1000px; max-width: 1500px; position: relative;">
                 <h1 class="abt-tk" style="font-weight: bold; position: relative; top: 5.5rem; color: #FFFFFF; right: 9rem;font-size: 96px;">About Us</h1>
-                <h2 class="tk-desc" style="max-width: 550px; position: relative; top: 7rem; color: #FFFFFF; right: 9rem; font-size: 22px;">PCMed-TechnoKids, conceptualized in 2002, is a premier computer curriculum service provider in the country specializing in delivering leading-edge computer curriculum to schools and providing excellent technology skills training to teachers in the service of the schools and their students.</h2>
+                <h2 class="tk-desc" style="max-width: 550px; position: relative; text-align: justify;  line-height: 1.8; top: 7rem; color: #FFFFFF; right: 9rem; font-size: 22px;">PCMed-TechnoKids, conceptualized in 2002, is a premier computer curriculum service provider in the country specializing in delivering leading-edge computer curriculum to schools and providing excellent technology skills training to teachers in the service of the schools and their students.</h2>
             </div>
         </div>
         </section>
         <div class="container" style="width: 1400px; max-width: 1400px; position: relative">
             <h1 class="mission" style="font-weight: bold; position: relative; top: 5.5rem; color: #FFFFFF; right: 5rem;font-size: 96px; margin-left:8rem;">Mission</h1>
-            <h3 class="tk-miss" style="max-width: 1370px; position: relative; top: 7rem; color: #FFFFFF; right: 5rem; font-size: 22px; margin-left:8rem;">We are committed to delivering the best information technology services and solutions in support of the teaching and needs of our clients and to improve the effectiveness and efficiency of digital technology by providing statewide leadership, oversight, products and services for activities related to information technology.</h2>
+            <h3 class="tk-miss" style="max-width: 1370px; position: relative; top: 7rem; color: #FFFFFF; right: 5rem; line-height: 1.8; text-align: justify; font-size: 22px; margin-left:8rem;">We are committed to delivering the best information technology services and solutions in support of the teaching and needs of our clients and to improve the effectiveness and efficiency of digital technology by providing statewide leadership, oversight, products and services for activities related to information technology.</h2>
         </div>
         <div class="container" style="width: 1400px; max-width: 1400px; position: relative">
             <h1 class="vision" style="font-weight: bold; position: relative; top: 12rem; color: #000000; right: 5rem;font-size: 96px; margin-left:8rem;">Vision</h1>
-            <h3 class="tk-vis" style="max-width: 1370px; position: relative; top: 13rem; color: #000000; right: 4rem; font-size: 22px; margin-left:8rem;">To be recognized as the preferred source of information technology services and solutions within the campus community and recognized as one of the leaders in the market innovative technology today.</h2>
+            <h3 class="tk-vis" style="max-width: 1370px; position: relative; top: 13rem; color: #000000; right: 4rem; line-height: 1.8; text-align: justify; font-size: 22px; margin-left:8rem;">To be recognized as the preferred source of information technology services and solutions within the campus community and recognized as one of the leaders in the market innovative technology today.</h2>
         </div>
+        
         <section style="background: url(&quot;assets/img/Group%2033527.svg&quot;) top / cover;height: 1200px;padding-top: 8rem;">
-            <div class="carousel slide" data-bs-ride="false" id="carousel-1" style="margin-right: auto;margin-left: auto;max-width: 850px;text-align: center;min-width: 330px; ">
-                <div class="carousel-inner" style="text-align: center;">
-                    <div class="carousel-item active" style="max-width: 850px;">
-                        <div style="height: 960px; position:relative;">
-                            <div id="slide-responsive" style="top: 23rem;background: #373737;position: absolute;border-radius: 15px;width: 100%;height: auto;">
-                                <h1 class="fw-normal" style="margin-top: 9rem;color: rgb(255,255,255);margin-left: 7rem;margin-right: 7rem;font-size: 20px;margin-bottom: 8rem;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec eros auctor, placerat odio et, sollicitudin sapien. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.&nbsp;</h1>
-                                <div style="left: 0;bottom: 0;background: rgba(31,31,31,0.9);border-bottom-right-radius: 15px;border-bottom-left-radius: 15px;position: absolute;width: 100%;">
-                                    <h1 style="color: rgb(255,255,255);font-size: 23px;margin-top: 10px;">Jinnah</h1>
-                                    <h1 class="fw-lighter" style="color: rgb(255,255,255);font-size: 21px;margin-top: 0px;">Position</h1>
-                                </div>
-                            </div><img class="rounded-circle img-fluid" src="assets/img/clipboard-image.png" alt="Slide Image" width="850" height="201" style="object-fit: cover;text-align: center;width: 200px;height: 200px;transform: perspective(0px) translateY(260px);">
-                        </div>
-                    </div>
-                    <div class="carousel-item" style="max-width: 850px;">
-                        <div style="height: 960px; position:relative;">
-                            <div id="slide-responsive" style="top: 23rem;background: #373737;position: absolute;border-radius: 15px;width: 100%;height: auto;">
-                                <h1 class="fw-normal" style="margin-top: 9rem;color: rgb(255,255,255);margin-left: 7rem;margin-right: 7rem;font-size: 20px;margin-bottom: 8rem;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et nunc lacus."</h1>
-                                <div style="left: 0;bottom: 0;background: rgba(31,31,31,0.9);border-bottom-right-radius: 15px;border-bottom-left-radius: 15px;position: absolute;width: 100%;">
-                                    <h1 style="color: rgb(255,255,255);font-size: 23px;margin-top: 10px;">FirstName Last Name</h1>
-                                    <h1 class="fw-lighter" style="color: rgb(255,255,255);font-size: 21px;margin-top: -14px;">Position</h1>
-                                </div>
-                            </div><img class="rounded-circle img-fluid" src="assets/img/18724b620dcf8ff579dcb99cb1599f21.webp" alt="Slide Image" width="850" height="201" style="object-fit: cover;text-align: center;width: 200px;height: 200px;transform: perspective(0px) translateY(260px);">
-                        </div>
-                    </div>
-                    <div class="carousel-item" style="max-width: 850px;">
-                        <div style="height: 960px; position:relative;">
-                            <div id="slide-responsive" style="top: 23rem;background: #373737;position: absolute;border-radius: 15px;width: 100%;height: auto;">
-                                <h1 class="fw-normal" style="margin-top: 9rem;color: rgb(255,255,255);margin-left: 7rem;margin-right: 7rem;font-size: 20px;margin-bottom: 8rem;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et nunc lacus."</h1>
-                                <div style="left: 0;bottom: 0;background: rgba(31,31,31,0.9);border-bottom-right-radius: 15px;border-bottom-left-radius: 15px;position: absolute;width: 100%;">
-                                    <h1 style="color: rgb(255,255,255);font-size: 23px;margin-top: 10px;">FirstName Last Name</h1>
-                                    <h1 class="fw-lighter" style="color: rgb(255,255,255);font-size: 21px;margin-top: -14px;">Position</h1>
-                                </div>
-                            </div><img class="rounded-circle img-fluid" src="assets/img/18724b620dcf8ff579dcb99cb1599f21.webp" alt="Slide Image" width="850" height="201" style="object-fit: cover;text-align: center;width: 200px;height: 200px;transform: perspective(0px) translateY(260px);">
-                        </div>
-                    </div>
-                </div>
-                <div><a class="carousel-control-prev" href="#carousel-1" role="button" data-bs-slide="prev"><span class="carousel-control-prev-icon"></span><span class="visually-hidden">Previous</span></a><a class="carousel-control-next" href="#carousel-1" role="button" data-bs-slide="next"><span class="carousel-control-next-icon"></span><span class="visually-hidden">Next</span></a></div>
-                <ol class="carousel-indicators" style="display: none;">
-                    <li data-bs-target="#carousel-1" data-bs-slide-to="0" class="active"></li>
-                    <li data-bs-target="#carousel-1" data-bs-slide-to="1"></li>
-                    <li data-bs-target="#carousel-1" data-bs-slide-to="2"></li>
-                </ol>
+        
+        <div class="container" style="width: 1400px; max-width: 1400px; position: relative">
+            <h1 class="vision" style="font-weight: bold; position: relative; top: 12rem; color: #000000; right: 5rem;font-size: 96px; margin-left:8rem;">School Partnerships </h1>
+            <h3 class="tk-vis" style="max-width: 1370px; position: relative; top: 13rem; color: #000000; right: 4rem; line-height: 1.8; text-align: justify; font-size: 22px; margin-left:8rem;">Establishing a school partnership, we aim to foster collaboration and create mutually beneficial initiatives that enhance educational opportunities for students.</h3>
+        </div>
+
+
+        <div class="container" style="width: 1000px; max-width: 1500px; position: relative; margin: 0 auto; top: 500px; text-align: center; display: grid; grid-template-columns: 1fr 1fr 1fr; gap: 90px;">
+            <div>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 15px; margin: 0 auto; color: black;"><b>FOUNDED</b></h2>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 40px; margin: 0 auto; color: black;"><strong>2002</strong></h2>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 15px; margin: 0 auto;"><b>I.T. Company</b></h2>
+            <br>
+            <br>
+            <br>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 15px; margin: 0 auto; color: black;"><b>PROVIDES</b></h2>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 40px; margin: 0 auto; color: black;"><strong>Computer</strong></h2>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 15px; margin: 0 auto;"><b>Curriculum</b></h2>
+            </div>
+
+            <div>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 15px; margin: 0 auto; color: black;"><b>HEADQUARTERS</b></h2>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 40px; margin: 0 auto; color: black;"><strong>Manila, PH</strong></h2>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 15px; margin: 0 auto;"><b>Legaspi Towers 300</b></h2>
+            <br>
+            <br>
+            <br>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 15px; margin: 0 auto; color: black;"><b>PROVIDES</b></h2>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 40px; margin: 0 auto; color: black;"><strong>Teachers</strong></h2>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 15px; margin: 0 auto;"><b>Technology Skills</b></h2>
+            </div>
+
+            <div>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 15px; margin: 0 auto; color: black;"><b>BRANCHES</b></h2>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 40px; margin: 0 auto; color: black;"><strong>2</strong></h2>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 15px; margin: 0 auto;"><b>Cebu & Davao City</b></h2>
+            <br>
+            <br>
+            <br>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 15px; margin: 0 auto; color: black;"><b>PROVIDES</b></h2>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 40px; margin: 0 auto; color: black;"><strong>Values</strong></h2>
+            <h2 class="tk-desc" style="max-width: 550px; font-size: 15px; margin: 0 auto;"><b>Our DNA</b></h2>
             </div>
         </section>
     </div>
@@ -95,6 +96,7 @@
     <script src="assets/js/Simple-Slider.js"></script>
     <script src="assets/js/Task-List.js"></script>
     <script src="assets/js/Ultimate-Event-Calendar.js"></script>
+    
     
 <!-- footer-->
 <?php include 'includes/footer.php'; ?>
