e7c610a672fffd1c3a04297b9ba4074e18dea869
Llivi Cai <138189751+Llivi-Jaaz@users.noreply.github.com>
2023-07-31 09:45:55 +0800

    Update indexnew.php
    Make all the 'join now' buttons in indexnew linked in to the signup page

--- indexnew.php
+++ indexnew.php
@@ -37,16 +37,77 @@
         <video class="parallax-background" autoplay="" loop="" muted="">
             <source src="landing-assets/video/cubes.mp4" type="video/mp4" wp-acf="[{'type':'url','name':'video','label':'Video','wrapper':{'width':25}},{'type':'text','name':'video_css','label':'Video CSS (eg. filters)','wrapper':{'width':25}}]" wp-attr="[{'target':'src','replace':'%1'},{'target':'parent_style','replace':'%2'}]">
         </video>
-    
     </div>
+
+
+
+
+
+    <div style = "background-color: #CDE5FE;">
+        <div class="container py-4 py-xl-5" style="width: 1040px; ">
+            <div class="row mb-5">
+                <div class="col-md-8 col-xl-6 text-center mx-auto">
+                    <h1 class="fw-semibold" style="width: auto;font-weight: bold; color:#0017EB"><strong>Featured Projects</strong></h1>
+                    <p style = "font-size: 18px";>Here are some of the projects done by the WISDOM team:</p>
+                </div>
+            </div>
+            <div class="row gy-4 row-cols-1 row-cols-md-2 row-cols-xl-3" style="margin-top: -57px;">
+                <div class="col"><a class="text-decoration-none" href="#" style="color: rgb(0,0,0);">
+                        <div class="card" style="border-radius: 15px;border-style: none;box-shadow: 0px 3px 8px 0px rgb(215,215,215);padding: 5px 10px;">
+                            <div class="card-body p-4"><img width="306" height="80" style="width: 100%;object-fit: cover;height: 130px;border-radius: 15px;" src="assets/img/technotyper.png">
+                                <h4 class="card-title" style="margin-top: 16px;font-weight: bold;">TechnoTyper</h4>
+                                <p class="card-text" style="font-size: 14px; text-align:justify;"><span style="background-color: transparent;">A keyboarding web application to be developed for the purpose of teaching children with ages 4 to 6 years old the QWERTY finger typing method through fun and educational activities that will help develop and master typing skills using a keyboard.</span></p>
+                            </div>
+                        </div>
+                    </a></div>
+                <div class="col"><a class="text-decoration-none" href="#" style="color: rgb(0,0,0);">
+                        <div class="card" style="border-radius: 15px;border-style: none;box-shadow: 0px 3px 8px 0px rgb(215,215,215);padding: 5px 10px;">
+                            <div class="card-body p-4"><img width="306" height="80" style="width: 100%;object-fit: cover;height: 130px;border-radius: 15px;" src="assets/img/original-0219a6b663e274b49d3c5816428c890a.webp">
+                                <h4 class="card-title" style="margin-top: 16px;font-weight: bold;">Project Name</h4>
+                                <p class="card-text" style="font-size: 14px; text-align:justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a interdum ipsum. Nulla non ultrices ex. Quisque sit amet diam ante. Nullam lacinia, libero semper egestas varius, erat mauris molestie nibh, non vehicula neque massa eu sem.</p>
+                            </div>
+                        </div>
+                    </a></div>
+                <div class="col"><a class="text-decoration-none" href="#" style="color: rgb(0,0,0);">
+                        <div class="card" style="border-radius: 15px;border-style: none;box-shadow: 0px 3px 8px 0px rgb(215,215,215);padding: 5px 10px;">
+                            <div class="card-body p-4"><img width="306" height="80" style="width: 100%;object-fit: cover;height: 130px;border-radius: 15px;" src="assets/img/original-d210c6d256c9abb879cd7600b7e5c276.webp">
+                                <h4 class="card-title" style="margin-top: 16px;font-weight: bold;"><strong>Project Name</strong></h4>
+                                <p class="card-text" style="font-size: 14px; text-align:justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a interdum ipsum. Nulla non ultrices ex. Quisque sit amet diam ante. Nullam lacinia, libero semper egestas varius, erat mauris molestie nibh, non vehicula neque massa eu sem.</p>
+                            </div>
+                        </div>
+                    </a></div>
+            </div>
+            </div>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <div id="abt-1" style="height: 100vh;background: #6ca3a1;">
         <section style="background: #262426;">
             <div class="container" style="height: 100%;background: #6ca3a1;">
                 <div class="row row-media">
                     <div class="col-md-6 column-text" style="color: rgb(255,255,255);">
                         <h2 class="text-center" style="color: rgb(17,74,72);font-family: Montserrat, sans-serif;font-weight: bold;">ROBOTICS</h2>
-                        <p class="text-justify" style="color: rgb(17,74,72);font-family: Montserrat, sans-serif;">We are a team of robotics experts who design and create innovative solutions for businesses and organizations, using our multidisciplinary approach to improve efficiency and achieve goals.</p>
-                        <p class="text-center"><a class="btn btn-primary" role="button" style="height: 40px;width: 108.5px;font-family: Montserrat, sans-serif;font-weight: bold;border-width: 0px;background: rgb(17,74,72);color: rgb(108,163,161);">Join Now</a></p>
+                        <p class="text-justify" style="color: rgb(17,74,72); text-align: justify; font-family: Montserrat, sans-serif;">We are a team of robotics experts who design and create innovative solutions for businesses and organizations, using our multidisciplinary approach to improve efficiency and achieve goals.</p>
+                        <p class="text-center"><a href="register.php" class="btn btn-primary" role="button" style="height: 40px;width: 108.5px;font-family: Montserrat, sans-serif;font-weight: bold;border-width: 0px;background: rgb(17,74,72);color: rgb(108,163,161);">Join Now</a></p>
                     </div>
                     <div class="col-md-6 column-media"><img class="img-fluid" data-aos="zoom-in" alt="Lorem" src="landing-assets/img/431fdf9cd1c08057a56c65607d0e6599.gif"></div>
                 </div>
@@ -59,8 +120,8 @@
                 <div class="row row-media">
                     <div class="col-md-6 column-text" style="color: rgb(255,255,255);">
                         <h2 class="text-center" style="color: rgb(88,48,34);font-family: Montserrat, sans-serif;font-weight: bold;">3D PRINTING</h2>
-                        <p class="text-justify" style="color: rgb(89,47,33);font-family: Montserrat, sans-serif;">We are a team of passionate animators who create visually stunning content using a wide range of animation techniques and software. Our goal is to bring our clients' visions to life through the magic of animation.</p>
-                        <p class="text-center"><a class="btn btn-primary" role="button" style="height: 40px;width: 108.5px;font-family: Montserrat, sans-serif;font-weight: bold;border-width: 0px;background: rgb(91,49,35);color: rgb(250,221,145);">Join Now</a></p>
+                        <p class="text-justify" style="color: rgb(89,47,33); text-align: justify; font-family: Montserrat, sans-serif;">We are a team of passionate animators who create visually stunning content using a wide range of animation techniques and software. Our goal is to bring our clients' visions to life through the magic of animation.</p>
+                        <p class="text-center"><a href="register.php" class="btn btn-primary" role="button" style="height: 40px;width: 108.5px;font-family: Montserrat, sans-serif;font-weight: bold;border-width: 0px;background: rgb(91,49,35);color: rgb(250,221,145);">Join Now</a></p>
                     </div>
                     <div class="col-md-6 column-media"><img class="img-fluid" data-aos="zoom-in" alt="Lorem" src="landing-assets/img/3d.gif"></div>
                 </div>
@@ -74,8 +135,8 @@
                     <div class="col-md-6 column-media"><img class="img-fluid" data-aos="zoom-in" alt="Lorem" style="border-color: rgba(33,37,41,0);" src="landing-assets/img/developers-gif-showcase.gif"></div>
                     <div class="col-md-6 column-text" style="color: rgb(255,255,255);">
                         <h2 class="text-center" style="font-family: Montserrat, sans-serif;color: rgb(95,107,221);"><strong>PROGRAMMING</strong></h2>
-                        <p class="text-justify" style="font-family: Montserrat, sans-serif;color: rgb(95,107,221);">We are a team of programming specialists who use our expertise to create custom software solutions for businesses and organizations, helping our clients succeed in today's rapidly evolving technological landscape.</p>
-                        <p class="text-center"><a class="btn btn-primary" role="button" style="color: rgb(188,204,254);background: rgb(95,107,221);width: 108.5px;height: 40px;font-family: Montserrat, sans-serif;border-width: 0px;font-weight: bold;">Join Now</a></p>
+                        <p class="text-justify" style="font-family: Montserrat, sans-serif; text-align: justify; color: rgb(95,107,221);">We are a team of programming specialists who use our expertise to create custom software solutions for businesses and organizations, helping our clients succeed in today's rapidly evolving technological landscape.</p>
+                        <p class="text-center"><a href="register.php" class="btn btn-primary" role="button" style="color: rgb(188,204,254);background: rgb(95,107,221);width: 108.5px;height: 40px;font-family: Montserrat, sans-serif;border-width: 0px;font-weight: bold;">Join Now</a></p>
                     </div>
                 </div>
             </div>
@@ -89,8 +150,8 @@
                     <div class="col-md-6 column-media"><img class="img-fluid" data-aos="zoom-in" alt="Lorem" src="landing-assets/img/1_MptjiGxY9vSRW4ZC9S7Isw.gif"></div>
                     <div class="col-md-6 column-text" style="color: rgb(255,255,255);">
                         <h2 class="text-center" style="font-family: Montserrat, sans-serif;color: rgb(194,159,242);"><strong>GRAPHIC DESIGN</strong></h2>
-                        <p class="text-justify" style="font-family: Montserrat, sans-serif;color: rgb(194,159,242);">We are a team of passionate animators who create visually stunning content using a wide range of animation techniques and software. Our goal is to bring our clients' visions to life through the magic of animation.</p>
-                        <p class="text-center"><a class="btn btn-primary" role="button" style="color: rgb(247,255,255);background: rgb(194,159,242);width: 108.5px;height: 40px;font-family: Montserrat, sans-serif;border-width: 0px;font-weight: bold;">Join Now</a></p>
+                        <p class="text-justify" style="font-family: Montserrat, sans-serif; text-align: justify; color: rgb(194,159,242);">We are a team of passionate animators who create visually stunning content using a wide range of animation techniques and software. Our goal is to bring our clients' visions to life through the magic of animation.</p>
+                        <p class="text-center"><a href="register.php" class="btn btn-primary" role="button" style="color: rgb(247,255,255);background: rgb(194,159,242);width: 108.5px;height: 40px;font-family: Montserrat, sans-serif;border-width: 0px;font-weight: bold;">Join Now</a></p>
                     </div>
                 </div>
             </div>
@@ -103,8 +164,8 @@
                     <div class="col-md-6 column-media"><img class="img-fluid" data-aos="zoom-in" alt="Lorem" src="landing-assets/img/ezgif.com-video-to-gif.gif"></div>
                     <div class="col-md-6 column-text" style="color: rgb(255,255,255);">
                         <h2 class="text-center" style="font-family: Montserrat, sans-serif;"><strong>ANIMATION</strong></h2>
-                        <p class="text-justify" style="font-family: Montserrat, sans-serif;">We are a team of passionate animators who create visually stunning content using a wide range of animation techniques and software. Our goal is to bring our clients' visions to life through the magic of animation.</p>
-                        <p class="text-center"><a class="btn btn-primary" role="button" style="color: rgb(38,36,38);background: rgb(255,255,255);width: 108.5px;height: 40px;font-family: Montserrat, sans-serif;border-width: 0px;font-weight: bold;">Join Now</a></p>
+                        <p class="text-justify" style="font-family: Montserrat, sans-serif; text-align: justify;">We are a team of passionate animators who create visually stunning content using a wide range of animation techniques and software. Our goal is to bring our clients' visions to life through the magic of animation.</p>
+                        <p class="text-center"><a href="register.php" class="btn btn-primary" role="button" style="color: rgb(38,36,38);background: rgb(255,255,255);width: 108.5px;height: 40px;font-family: Montserrat, sans-serif;border-width: 0px;font-weight: bold;">Join Now</a></p>
                     </div>
                 </div>
             </div>
