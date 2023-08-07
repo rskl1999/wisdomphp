9140af5df913f09e632f25544ce87180f1a8a9ed
Justine Marie Dusaran <84701462+justinedusaran@users.noreply.github.com>
2023-08-07 15:40:17 +0800

    Update FacilitatorStudentList.php
    - updated UI

--- Facilitator/FacilitatorStudentList.php
+++ Facilitator/FacilitatorStudentList.php
@@ -168,6 +168,8 @@
         </section>
     <script src="facilitator-assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="../logout.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="scripts/nav.js"></script>
 </body>
 
 </html>
